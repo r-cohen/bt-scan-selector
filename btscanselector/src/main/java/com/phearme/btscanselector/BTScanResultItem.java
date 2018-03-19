638084f9eba3bbe52aa9f4ba5a7eb349c3542890
raphael <raphael.cohen@gmail.com>
2018-03-19 10:02:01 +0200

    display rssi and signal level, sort by rssi

--- btscanselector/src/main/java/com/phearme/btscanselector/BTScanResultItem.java
+++ btscanselector/src/main/java/com/phearme/btscanselector/BTScanResultItem.java
@@ -28,19 +28,23 @@ public class BTScanResultItem {
     }
 
     private void updateIconResId() {
-        if (rssi < -25 && rssi > -50) {
+        if (rssi > -50) {
+            iconResourceId = R.drawable.network_high;
+            return;
+        }
+        if (rssi <= -50 && rssi > -67) {
             iconResourceId = R.drawable.network_medium;
             return;
         }
-        if (rssi <= -50 && rssi > -75) {
+        if (rssi <= -67 && rssi > -70) {
             iconResourceId = R.drawable.network_low;
             return;
         }
-        if (rssi < -75) {
+        if (rssi <= -70) {
             iconResourceId = R.drawable.network_no_signal;
             return;
         }
-        iconResourceId = R.drawable.network_high;
+        iconResourceId = R.drawable.network_null;
     }
 
     public int getIconResourceId() {
